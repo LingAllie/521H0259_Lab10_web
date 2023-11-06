6e9d55b25635b5c58a8a2949c689a657c6d0f76c
Jenny Thai <jennythai.fw@gmail.com>
2023-11-06 15:02:43 +0700

    "done 2p18 clip 3"

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-require('dotenv').config()
-const express = require('express')
-const socketio = require('socket.io')
+require("dotenv").config();
+const express = require("express");
+const socketio = require("socket.io");
 
-const app = express()
-app.set('view engine', 'ejs')
+const app = express();
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/login', (req, res) => {
-    res.render('login')
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get('/chat', (req, res) => {
-    res.render('chat')
-})
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
 
-const PORT = process.env.PORT || 1050
-const httpServer = app.listen(PORT, () => console.log('http://localhost:' + PORT))
-const io = socketio(httpServer)
+const PORT = process.env.PORT || 1050;
+const httpServer = app.listen(PORT, () =>
+  console.log("http://localhost:" + PORT)
+);
+const io = socketio(httpServer);
 
-io.on('connection', client => {
-    console.log(`Client ${client.id} connected`);
+io.on("connection", (client) => {
+  console.log(`Client ${client.id} connected`);
 
-    client.on('disconnect', () => console.log(`${client.id} has left`))
-})
+  let users = Array.from(io.sockets.sockets.values()).map((socket) => ({
+    id: socket.id,
+    name: socket.name,
+  }));
+  console.log(users);
+  client.on("disconnect", () => console.log(`/t/t${client.id} has left`));
+  client.emit("list-users", users);
+});
