bebe0a756e50f4c7cefb98fd635d5ae49a34d0b5
LingAllie <132822814+LingAllie@users.noreply.github.com>
2023-11-06 13:59:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/chat', (req, res) => {
+    res.render('chat')
+})
+
+const PORT = process.env.PORT || 1050
+const httpServer = app.listen(PORT, () => console.log('http://localhost:' + PORT))
+const io = socketio(httpServer)
+
+io.on('connection', client => {
+    console.log(`Client ${client.id} connected`);
+
+    client.on('disconnect', () => console.log(`${client.id} has left`))
+})
