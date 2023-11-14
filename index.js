21e2e0576143c2b3c46a7a49a9e0f9b52d79ba07
LingAllie <132822814+LingAllie@users.noreply.github.com>
2023-11-14 23:27:45 +0700

    done video 5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const { name } = require('ejs')
 const express = require('express')
 const socketio = require('socket.io')
 
@@ -24,5 +25,28 @@ const io = socketio(httpServer)
 io.on('connection', client => {
     console.log(`Client ${client.id} connected`);
 
-    client.on('disconnect', () => console.log(`${client.id} has left`))
+    let users = Array.from(io.sockets.sockets.values())
+        .map(socket => ({id: socket.id, name: socket.username}))
+    console.log(users);
+
+    client.on('disconnect', () => {
+        console.log(`\t\t${client.id} has left`)
+
+        //inform all remain users before we leave
+        client.broadcast.emit('user-leave', client.id);
+    })
+
+    client.on('register-name', username => {
+        client.username = username
+
+        // send register information to all remained users
+        client.broadcast.emit('register-name', {id: client.id, username: username})
+    })
+
+    //send list of users are online for new user
+    client.emit('list-users', users)
+
+    //send new user information for all previous users
+    client.broadcast.emit('new-user', {id: client.id, name: client.name})
+
 })
