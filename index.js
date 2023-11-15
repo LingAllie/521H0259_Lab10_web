000f33b84658bbef5d7e565dd511e644a27ed75a
LingAllie <132822814+LingAllie@users.noreply.github.com>
2023-11-15 13:03:04 +0700

    video7

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 require('dotenv').config()
+const { name } = require('ejs')
 const express = require('express')
 const socketio = require('socket.io')
+
 
 const app = express();
 app.set("view engine", "ejs");
@@ -26,5 +29,32 @@ const io = socketio(httpServer);
 io.on("connection", (client) => {
   console.log(`Client ${client.id} connected`);
 
-    client.on('disconnect', () => console.log(`${client.id} has left`))
+  client.free = true;
+  client.loginAt = new Date().toLocaleTimeString();
+
+
+  
+
+  client.on('disconnect', () => {
+      console.log(`\t\t${client.id} has left`)
+      //inform all remain users before we leave
+      client.broadcast.emit('user-leave', client.id);
+  })
+
+  client.on('register-name', username => {
+      client.username = username
+      // send register information to all remained users
+      client.broadcast.emit('register-name', {id: client.id, username: username})
+  })
+
+  //send list of users are online for new user
+  client.emit('list-users', Array.from(io.sockets.sockets.values())
+  .map(socket => ({id: socket.id, username: socket.username, free: socket.free, loginAt: socket.loginAt})))
+
+
+  //send new user information for all previous users
+  client.broadcast.emit('new-user', {id: client.id, username: client.username, free: client.free,
+      loginAt: client.loginAt})
+
 })
+
